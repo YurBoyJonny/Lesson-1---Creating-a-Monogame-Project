f198de3dc3893083a6b0f9177d912942e5825af3
YurBoyJonny <CheuJona853@GoTVDSB.ca>
2022-06-11 13:13:51 -0400

    done

--- Lesson1.cs
+++ Lesson1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Lesson_1
 {
@@ -11,7 +12,21 @@ namespace Lesson_1
         private SpriteBatch _spriteBatch;
 
         Texture2D ballTexture;
-        Texture2D guyinsuitTexture;
+        Rectangle ballRect;
+
+        Texture2D backgroundTexture;
+        Rectangle backgroundRect;
+
+        Texture2D walterwhiteTexture;
+        Rectangle walterwhiteRect;
+
+        Texture2D dinoTexture;
+
+        Texture2D basketballTexture;
+        Rectangle basketballRect;
+
+        int i;
+        Random generator = new Random();
 
         public Lesson1()
         {
@@ -26,6 +41,15 @@ namespace Lesson_1
             _graphics.PreferredBackBufferHeight = 500; // Sets the height of the window
             _graphics.ApplyChanges(); // Applies the new dimensions
             this.Window.Title = "My First Monogame Project";
+
+            backgroundRect = new Rectangle(0, 0, 800, 500);
+
+            walterwhiteRect = new Rectangle(600, 300, 200, 300);
+
+            basketballRect = new Rectangle(100, 100, 100, 100);
+
+            ballRect = new Rectangle(100, 100, 100, 100);
+
             base.Initialize();
         }
         protected override void LoadContent()
@@ -34,7 +58,19 @@ namespace Lesson_1
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             // TODO: use this.Content to load your game content here
             ballTexture = Content.Load<Texture2D>("Balls");
-            guyinsuitTexture = Content.Load<Texture2D>("GuyInSuit");
+            ballRect = new Rectangle(generator.Next(1,600), generator.Next(1,500), 100, 100);
+
+            backgroundTexture = Content.Load<Texture2D>("breakingbadbackground");
+            backgroundRect = new Rectangle(0, 0, 800, 500);
+
+            walterwhiteTexture = Content.Load<Texture2D>("walterwhite");
+            walterwhiteRect = new Rectangle(600, 300, 200, 300);
+
+            dinoTexture = Content.Load<Texture2D>("dino");
+
+            basketballTexture = Content.Load<Texture2D>("basketball");
+            basketballRect = new Rectangle(generator.Next(1,600), generator.Next(1,500), 100, 100);
+
         }
         protected override void Update(GameTime gameTime)
         {
@@ -49,8 +85,18 @@ namespace Lesson_1
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(ballTexture, new Vector2(0, 0), Color.White);
-            _spriteBatch.Draw(guyinsuitTexture, new Vector2(0, 0), Color.White);
+            _spriteBatch.Draw(backgroundTexture,backgroundRect, Color.White);
+
+            for (i = 0; i <= 800; i += 100)
+            {
+                _spriteBatch.Draw(dinoTexture, new Vector2(i,50), Color.White);
+            }
+
+            _spriteBatch.Draw(walterwhiteTexture, walterwhiteRect, Color.White);
+
+            _spriteBatch.Draw(ballTexture, ballRect, Color.White);
+
+            _spriteBatch.Draw(basketballTexture, basketballRect, Color.White);
 
             _spriteBatch.End();
             base.Draw(gameTime);
